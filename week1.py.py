4b912c80c26cf22895fdbfe38352ea9be21ffb01
WejdanAyed <WejdanAyed@users.noreply.github.com>
2019-09-04 11:15:48 +0300

    week1

---
+++ week1.py.py
@@ -0,0 +1,59 @@
+#Week1 , Day1
+print("Hello, world") #python
+
+#Week1 , Day2
+#print("WejdanAyed")
+print("WejdanAyed")
+"""
+Hello world
+this is not a code
+but is a line comment just
+"""
+print("100DayOfCode")
+
+#Week1 , Day3
+x = y = z = "WejdanAyed" #All variables have same value
+print(x)
+print(y)
+print(z)
+value1 =5 #x is of type int
+value2 ="John" #x is of type str
+value3 =8 #x is not of type str
+print(value1)
+print(value2)
+V1 = 5011
+V2 = 678
+V3 = 32
+print( V1 + V2 + V3 )
+
+
+#Week1 , Day4
+M1= 5 #int
+M2 = 3.77 #float
+M3= 1j#complex
+print(type(M1))
+print(type(M2))
+print(type(M3))
+a = float(M1) #convert from int to float
+b = int(M2) #convert from float to int
+c = complex (M1) #convert from int to complex
+print(type(a))
+print(type(b))
+print(type(c))
+import random
+print(random.randrange(33,44))
+
+#Week1 , Day5
+x , y ,z = "apple" , "orange" , "limon"
+basket = x + y + z
+print(basket)
+
+
+
+
+
+
+
+
+
+
